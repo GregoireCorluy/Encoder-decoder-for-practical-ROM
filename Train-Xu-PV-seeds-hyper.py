a44d5dfac5cf75ad505ced0fac58e4df2cb8251f
GregoireCorluy <gcorluy@gmail.com>
2026-07-30 17:17:43 +0200

    update the data for mixture fraction 0 to 1 for each simulation and add missing simulation to temperature data, launch new training with updated data

--- Train-Xu-PV-seeds-hyper.py
+++ Train-Xu-PV-seeds-hyper.py
@@ -60,7 +60,7 @@ perc_val = 0.1 #0.2 #percentage of validation data
 list_species_input = ['H2', 'H', 'O', 'O2', 'OH', 'H2O', 'HO2', 'H2O2', 'C', 'CH', 'CH2', 'CH2S', 'CH3', 'CH4', 'CO', 'CO2', 'HCO', 'CH2O', 'CH2OH', 'CH3O', 'CH3OH', 'C2H', 'C2H2', 'C2H3', 'C2H4', 'C2H5', 'C2H6', 'HCCO', 'CH2CO', 'HCCOH', 'C3H7', 'C3H8', 'CH2CHO', 'CH3CHO', 'N2']
 list_species_output_evaluation = ["CH4", "CO", "O2", "CO2", "H2O", "N2"]
 input_scaling_name = "None"
-temperature_output = False
+temperature_output = True
 output_scaling = "-1to1"
 
 # Encoder-decoder architecture
@@ -126,7 +126,7 @@ for idxConfig, config in enumerate(experiment_configs):
     species_tag = config["species_tag"]
     my_seed = config["seed"]
 
-    training_nbr = f"1aFirstTry_{optimizer_name}_{int(lr*10000)}_{species_tag}"
+    training_nbr = f"1bUpdatedData_{optimizer_name}_{int(lr*10000)}_{species_tag}"
     training_id = f"Tr{training_nbr}_s{my_seed}"
     list_ids.append(training_id)
     print(training_id)
